aa34b57abaa15c4aa86d377436bba41135ff7c74
ash322ash422 <160450942+ash322ash422@users.noreply.github.com>
2025-09-13 13:01:17 +0530

    updated

--- tut_py_4_0_module.py
+++ tut_py_4_0_module.py
@@ -1,9 +1,11 @@
 import math
 # you import math to get additional functionalities, like factorial, summation, celing, etc
+# factorial definition: 10! = 10 * 9 * 8 * 7 * ..2 * 1
 
 res_fact = math.factorial(10)
 print(f"Factorial = {res_fact}")
 
 
 res_sqrt = math.sqrt(110)
-print(f"is sqaure root = {res_sqrt}")
+print(f"Square root = {res_sqrt}")
+
